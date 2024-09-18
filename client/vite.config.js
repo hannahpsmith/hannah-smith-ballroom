2bc3e618f374f00ea35f79595b8f868380da9a92
Acid0001 <pissboyree@gmail.com>
2024-09-17 21:13:29 -0700

    Adding completed mini project code

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+    server: {
     port: 3000,
     open: true,
     proxy: {
-      // Uncomment the following code once you have built the queries and mutations in the client folder
-      // '/graphql': {
-      // Comment out the following code once you have built the queries and mutations in the client folder
-      '/api': {
+      '/graphql': {
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
