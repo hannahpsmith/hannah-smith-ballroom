974baaf66409cd8dfa10b6ec9b4f3ba93dbc97f4
Eduardo Velez <166083895+Massogon@users.noreply.github.com>
2024-09-19 21:08:51 -0700

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About HSB</h1>
+      <p>Information about HSB...</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/weddings/Packages.jsx
@@ -0,0 +1,11 @@
+const Packages = () => {
+    return (
+      <div>
+        <h1>Wedding Packages</h1>
+        <p>Details about wedding packages...</p>
+      </div>
+    );
+  };
+  
+  export default Packages;
+  
