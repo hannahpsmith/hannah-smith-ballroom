d0e0de2b2ede4fed1a02fb07d2f8b408e2413f5a
Hannah <166345156+hannahpsmith@users.noreply.github.com>
2024-09-21 15:59:50 -0600

    commented out necessary paths to run app, replaced logos with png

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,10 @@ import About from './pages/About';
 import Notes from './pages/Notes';
 import Contact from './pages/Contact';
 import Login from './pages/Login';
-import Packages from './pages/weddings/Packages';
-import Gallery from './pages/weddings/Gallery';
-import Classes from './pages/learn-to-dance/Classes';
-import Schedule from './pages/learn-to-dance/Schedule';
+// import Packages from './pages/weddings/Packages';
+// import Gallery from './pages/weddings/Gallery';
+// import Classes from './pages/learn-to-dance/Classes';
+// import Schedule from './pages/learn-to-dance/Schedule';
 import NotFound from './pages/Contact';  // For handling 404 errors
 
 const router = createBrowserRouter([
@@ -35,23 +35,23 @@ const router = createBrowserRouter([
         element: <Login />
       },
       // Dropdown menu for Weddings section
-      {
-        path: 'weddings/packages',
-        element: <Packages />
-      },
-      {
-        path: 'weddings/gallery',
-        element: <Gallery />
-      },
-      // Dropdown menu for Learn to Dance section
-      {
-        path: 'learn-to-dance/classes',
-        element: <Classes />
-      },
-      {
-        path: 'learn-to-dance/schedule',
-        element: <Schedule />
-      }
+      // {
+      //   path: 'weddings/packages',
+      //   element: <Packages />
+      // },
+      // {
+      //   path: 'weddings/gallery',
+      //   element: <Gallery />
+      // },
+      // // Dropdown menu for Learn to Dance section
+      // {
+      //   path: 'learn-to-dance/classes',
+      //   element: <Classes />
+      // },
+      // {
+      //   path: 'learn-to-dance/schedule',
+      //   element: <Schedule />
+      // }
     ],
   },
 ]);
