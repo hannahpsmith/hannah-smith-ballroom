b4035c564d19aed42aaec59a719136c7accdb2c2
Hannah <166345156+hannahpsmith@users.noreply.github.com>
2024-09-21 18:34:41 -0600

    began nav styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,32 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { Box, Flex, Link, Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
+import { Box, Flex, Link, Menu, MenuButton, MenuList, MenuItem, Button, textDecoration } from '@chakra-ui/react';
 
 export default function Navbar() {
+  const linkStyles = {
+    bg: "brand.400",
+    color: "brand.50",
+  }
   return (
-    <Flex as="nav" className="navbar" justify="space-between" align="center" p={4} bg="gray.100">
-      <Box className="navbar-left">
-        <Link as={RouterLink} to="/login" className="login-btn" color="teal.500">
+    <Flex as="nav" className="navbar" justify="space-between" align="top" p={14} bg={"brand.900"} >
+      <Box className="navbar-right" position="absolute" top={2} right={2}>
+        <Link as={Button} to="/login" className="login-btn" bg="brand.300" color="brand.50">
           Login
         </Link>
       </Box>
 
-      <Flex className="navbar-links" gap={4}>
-        <Link as={RouterLink} to="/about" color="teal.500">
+      <Flex className="navbar-links" gap={4} position="absolute" bottom={10} left="50%" transform="translateX(-50%)">
+        <Link as={Button} to="/about" {...linkStyles} >
           About HSB
         </Link>
-        <Link as={RouterLink} to="/notes" color="teal.500">
+        <Link as={Button} to="/notes" {...linkStyles} >
           Notes
         </Link>
-        <Link as={RouterLink} to="/contact" color="teal.500">
+        <Link as={Button} to="/contact" {...linkStyles}>
           Contact
         </Link>
 
         <Menu>
-          <MenuButton as={Button} colorScheme="teal">
+          <MenuButton as={Button} {...linkStyles} >
             Weddings
           </MenuButton>
           <MenuList>
@@ -36,7 +40,7 @@ export default function Navbar() {
         </Menu>
 
         <Menu>
-          <MenuButton as={Button} colorScheme="teal">
+          <MenuButton as={Button} {...linkStyles}>
             Learn to Dance
           </MenuButton>
           <MenuList>
