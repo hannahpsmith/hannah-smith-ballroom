886de938582ba8a4e4b32f12c485da67107eb2f8
Hannah <166345156+hannahpsmith@users.noreply.github.com>
2024-09-21 19:14:16 -0600

    continued nav styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,32 +1,39 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Box, Flex, Link, Menu, MenuButton, MenuList, MenuItem, Button, textDecoration } from '@chakra-ui/react';
+// import logo from "../../src/assets/logo-gold.png";
 
 export default function Navbar() {
   const linkStyles = {
     bg: "brand.400",
     color: "brand.50",
+    _hover: { 
+      bg: "brand.200",
+    },
+    _active: {
+      bg: "brand.300",
+    },
   }
   return (
     <Flex as="nav" className="navbar" justify="space-between" align="top" p={14} bg={"brand.900"} >
       <Box className="navbar-right" position="absolute" top={2} right={2}>
-        <Link as={Button} to="/login" className="login-btn" bg="brand.300" color="brand.50">
+        <Button as={RouterLink} to="/login" className="login-btn" bg="brand.300" color="brand.50"  _hover={{ bg: "brand.50", color: "brand.300" }}>
           Login
-        </Link>
+        </Button>
       </Box>
 
       <Flex className="navbar-links" gap={4} position="absolute" bottom={10} left="50%" transform="translateX(-50%)">
-        <Link as={Button} to="/about" {...linkStyles} >
+        <Button as={RouterLink} to="/about" {...linkStyles} >
           About HSB
-        </Link>
-        <Link as={Button} to="/notes" {...linkStyles} >
+        </Button>
+        <Button as={RouterLink} to="/notes" {...linkStyles} >
           Notes
-        </Link>
-        <Link as={Button} to="/contact" {...linkStyles}>
+        </Button>
+        <Button as={RouterLink} to="/contact" {...linkStyles}>
           Contact
-        </Link>
+        </Button>
 
         <Menu>
-          <MenuButton as={Button} {...linkStyles} >
+          <MenuButton as={Button} {...linkStyles}>
             Weddings
           </MenuButton>
           <MenuList>
