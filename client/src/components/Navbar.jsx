381db61fe0099c6c433366a0ed3e72d7185bfad3
MrCows <49926119+MrCows@users.noreply.github.com>
2024-09-23 12:07:10 -0700

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
     // <Flex as="nav" className="navbar" justify="space-between" align="center" p={{ base: 7, md: 8 }} bg={"brand.900"} position={'relative'} minHeight={{ base: '300px', md: '80px' }}>
     <Flex as="nav" className="navbar" bg={"brand.900"} justify="space-between" align="center" position='relative' p={{ base: 7, md: 10 }} minHeight={{ base: '360px', md: '80px' }}>
         <Flex justify={['center', 'space-between']} >
-          <Image src={logo} height={20} minW={265} obectFit='cover' />
+          <Image src={logo} height={20} minW={265} objectFit='cover' />
         </Flex>
       <Box className="navbar-right" position="absolute" top={3} right={3}>
         <Button as={RouterLink} to="/login" className="login-btn" {...loginStyles}>
