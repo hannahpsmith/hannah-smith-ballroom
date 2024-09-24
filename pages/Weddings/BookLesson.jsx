403a735dc82cf365df051748160907a5e04a2a7d
Eduardo Velez <166083895+Massogon@users.noreply.github.com>
2024-09-23 21:50:10 -0700

    Add files via upload

---
+++ pages/Weddings/BookLesson.jsx
@@ -0,0 +1,18 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Flex, Button } from '@chakra-ui/react';
+
+
+export default function WeddingBook() {
+  
+  return (
+    <Flex>
+        <Button as={RouterLink} to="/book-lesson/orem" >
+          Orem, UT
+        </Button>
+
+        <Button as={RouterLink} to="/book-lesson/slc">
+          SLC, UT
+        </Button>
+      </Flex>
+  );
+}
