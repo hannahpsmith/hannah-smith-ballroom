183bacd01a5299f005bddc3ea03451bdaadd0881
Acid0001 <pissboyree@gmail.com>
2024-09-24 13:20:55 -0700

    fixed widgets and fixe some duplicate stylesheet loading

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar';
-import { Box, Container,  } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import CalendlyBadgeOrem from './CalendlyOrem';
 import CalendlyBadgeSLC from './CalendlySLC';
 import Footer from './Footer';
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.href = 'https://assets.calendly.com/assets/external/widget.css';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
   return (
     <Box display="flex" flexDirection="column" minHeight="100vh">
       <Navbar />
@@ -15,7 +25,7 @@ export default function Layout({ children }) {
 
       <CalendlyBadgeSLC />
       <CalendlyBadgeOrem />
-      
+
       <Footer />
     </Box>
   );
--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -1,4 +1,17 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Notes = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
     <div>
       <h1>Notes</h1>
